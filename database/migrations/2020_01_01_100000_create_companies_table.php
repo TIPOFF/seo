8e40abf7743d2f346cd207c3884c44475205589d
drewroberts <github@drewroberts.com>
2021-02-23 10:43:18 -0500

    Create stub migration for companies #13

---
+++ database/migrations/2020_01_01_100000_create_companies_table.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Tipoff\Support\Enums\AppliesTo;
+
+class CreateCompaniesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+
+            $table->foreignIdFor(app('user'), 'creator_id')->nullable();
+            $table->foreignIdFor(app('user'), 'updater_id')->nullable();
+            $table->timestamps();
+        });
+    }
+}
--- database/migrations/2020_01_01_110000_create_keywords_table.php
+++ database/migrations/2020_01_01_110000_create_keywords_table.php
@@ -12,11 +12,11 @@ class CreateKeywordsTable extends Migration
 {
     public function up()
     {
-        Schema::create('discounts', function (Blueprint $table) {
+        Schema::create('keywords', function (Blueprint $table) {
             $table->id();
             $table->string('phrase')->index()->unique();
 
-            $table->foreignIdFor(Keyword::class, 'parent_id');
+            $table->foreignIdFor(Keyword::class, 'parent_id')->nullable();
             $table->foreignIdFor(app('user'), 'creator_id');
             $table->foreignIdFor(app('user'), 'updater_id');
             $table->timestamps();

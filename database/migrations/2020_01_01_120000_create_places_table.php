8ac7fa2a95b6b940a6719188f9e4dbe34f716820
drewroberts <github@drewroberts.com>
2021-02-23 12:01:08 -0500

    Create stub migration for places #15

---
+++ database/migrations/2020_01_01_120000_create_places_table.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Tipoff\Support\Enums\AppliesTo;
+
+class CreatePlacesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('places', function (Blueprint $table) {
+            $table->id();
+            $table->string('place_location')->unique(); // Google Places ID. Would prefer different field name.
+            $table->string('name')->nullable();
+
+            // Add nullable company_id
+            $table->foreignIdFor(app('user'), 'creator_id')->nullable();
+            $table->foreignIdFor(app('user'), 'updater_id')->nullable();
+            $table->timestamps();
+        });
+    }
+}

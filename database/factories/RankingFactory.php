6be7f896db214d338525afd75c9c3ce8a45b1e0e
drewroberts <github@drewroberts.com>
2021-02-24 09:47:23 -0500

    Add new rankings fields to factory #18

--- database/factories/RankingFactory.php
+++ database/factories/RankingFactory.php
@@ -7,6 +7,7 @@ namespace Tipoff\Seo\Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Tipoff\Seo\Models\Keyword;
 use Tipoff\Seo\Models\Ranking;
+use Tipoff\Seo\Models\Webpage;
 
 class RankingFactory extends Factory
 {
@@ -19,6 +20,16 @@ class RankingFactory extends Factory
             'provider'          => $this->faker->name,
             'keyword_id'        => Keyword::factory()->create()->id,
             'date'              => $this->faker->date('Y-m-D'),
+            'position_01'       => randomOrCreate(Webpage::class),
+            'position_02'       => randomOrCreate(Webpage::class),
+            'position_03'       => randomOrCreate(Webpage::class),
+            'position_04'       => randomOrCreate(Webpage::class),
+            'position_05'       => randomOrCreate(Webpage::class),
+            'position_06'       => randomOrCreate(Webpage::class),
+            'position_07'       => randomOrCreate(Webpage::class),
+            'position_08'       => randomOrCreate(Webpage::class),
+            'position_09'       => randomOrCreate(Webpage::class),
+            'position_10'       => randomOrCreate(Webpage::class),
             'creator_id'        => randomOrCreate(app('user')),
             'updater_id'        => randomOrCreate(app('user')),
         ];

d7059885d77db4b6948816d31c88d3dd0e8506c1
drewroberts <github@drewroberts.com>
2021-02-25 17:03:12 -0500

    Add fields to place details #22

--- database/migrations/2020_01_01_121000_create_place_details_table.php
+++ database/migrations/2020_01_01_121000_create_place_details_table.php
@@ -14,7 +14,17 @@ class CreatePlaceDetailsTable extends Migration
         Schema::create('place_details', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(Place::class);
-            $table->string('name')->nullable();
+            $table->string('name');
+            $table->date('opened_at')->nullable();
+            $table->string('address')->nullable();
+            $table->string('address2')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->string('zip', 5)->nullable();
+            $table->string('phone', 25)->nullable();
+            $table->string('maps_url')->nullable(); // URL for place's Google Maps page.
+            $table->string('latitude')->nullable();
+            $table->string('longitude')->nullable();
 
             $table->foreignIdFor(app('user'), 'creator_id')->nullable(); // User that requested the place details to be updated
             $table->timestamp('created_at');

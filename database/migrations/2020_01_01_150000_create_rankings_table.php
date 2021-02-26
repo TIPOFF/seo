87b9b3e8b9b0e9243ca3d7a23e00f8452bb8fd17
drewroberts <github@drewroberts.com>
2021-02-26 14:35:18 -0500

    Create migration for ranking results #25

--- database/migrations/2020_01_01_150000_create_rankings_table.php
+++ database/migrations/2020_01_01_150000_create_rankings_table.php
@@ -6,8 +6,6 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Tipoff\Seo\Models\Keyword;
-use Tipoff\Seo\Models\Place;
-use Tipoff\Seo\Models\Webpage;
 
 class CreateRankingsTable extends Migration
 {
@@ -15,31 +13,13 @@ class CreateRankingsTable extends Migration
     {
         Schema::create('rankings', function (Blueprint $table) {
             $table->id();
-            $table->string('engine')->index(); // Example: 'google', 'bing' (haha)
+            $table->string('engine')->index(); // Example: 'google', 'youtube', 'bing' (haha)
             $table->string('provider')->index(); // Example: 'serpapi', 'moz', 'ahrefs'
             $table->foreignIdFor(Keyword::class)->index();
             $table->date('date')->index();
 
-            // Results (Positions, Rankings, Organic Listings)
-            // Relations to 10 webpages. Trying to decide naming conventions and how to handle rank 0
-            $table->foreignIdFor(Webpage::class, 'position_01');
-            $table->foreignIdFor(Webpage::class, 'position_02');
-            $table->foreignIdFor(Webpage::class, 'position_03');
-            $table->foreignIdFor(Webpage::class, 'position_04');
-            $table->foreignIdFor(Webpage::class, 'position_05');
-            $table->foreignIdFor(Webpage::class, 'position_06');
-            $table->foreignIdFor(Webpage::class, 'position_07');
-            $table->foreignIdFor(Webpage::class, 'position_08');
-            $table->foreignIdFor(Webpage::class, 'position_09');
-            $table->foreignIdFor(Webpage::class, 'position_10');
-
-            // Local Listings
-            $table->foreignIdFor(Place::class, 'local_01')->nullable();
-            $table->foreignIdFor(Place::class, 'local_02')->nullable();
-            $table->foreignIdFor(Place::class, 'local_03')->nullable();
-
             $table->foreignIdFor(app('user'), 'creator_id')->nullable(); // Probably not ever used here, but just in case it is a requested run
-            $table->foreignIdFor(app('user'), 'updater_id')->nullable(); // THere may later be a few fields that are updatable, but most will be locked to not be editable
+            $table->foreignIdFor(app('user'), 'updater_id')->nullable(); // There may later be a few fields that are updatable, but most will be locked to not be editable
             $table->timestamps();
 
             $table->unique(['engine', 'provider', 'keyword_id', 'date']);

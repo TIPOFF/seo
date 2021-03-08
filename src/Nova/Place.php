8b5bedcf7c9a225b175ccb1fabe9cbacf7aa9678
devjk1 <jinsik@omniadigital.io>
2021-03-08 14:56:34 -0500

    [TGER-95] 45 add nova resource, Place

---
+++ src/Nova/Place.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tipoff\Seo\Nova;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Http\Requests\NovaRequest;
+use Laravel\Nova\Panel;
+use Tipoff\Support\Nova\BaseResource;
+
+class Place extends BaseResource
+{
+    public static $model = \Tipoff\Seo\Models\Place::class;
+
+    public static $title = 'name';
+
+    public static $search = [
+        'id',
+    ];
+
+    public function fieldsForIndex(NovaRequest $request)
+    {
+        return array_filter([
+            ID::make()->sortable(),
+        ]);
+    }
+
+    public function fields(Request $request)
+    {
+        return array_filter([
+            Text::make('Place location')->required()->creationRules('unique:places,place_location'),
+            Text::make('Name')->nullable(),
+
+            nova('webpage') ? BelongsTo::make('Webpage', 'webpage', nova('webpage'))->nullable() : null,
+            nova('company') ? BelongsTo::make('Company', 'company', nova('company'))->nullable() : null,
+
+            new Panel('Data Fields', $this->dataFields()),
+        ]);
+    }
+
+    public function dataFields(): array
+    {
+        return array_merge(
+            parent::dataFields(),
+            [
+                $this->creatorDataFields(),
+                $this->updaterDataFields(),
+            ]
+        );
+    }
+}

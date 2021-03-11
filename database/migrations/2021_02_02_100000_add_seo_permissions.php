91687a9ce92b288956d9aab601e8cf66015e794b
Drew Roberts <github@drewroberts.com>
2021-03-10 19:55:11 -0500

    Update package permissions migration #32 #47

--- database/migrations/2021_02_02_100000_add_seo_permissions.php
+++ database/migrations/2021_02_02_100000_add_seo_permissions.php
@@ -8,38 +8,34 @@ class AddSeoPermissions extends BasePermissionsMigration
 {
     public function up()
     {
-        if (app()->has(Permission::class)) {
-            app(PermissionRegistrar::class)->forgetCachedPermissions();
+        $permissions = [
+            'view companies' => ['Owner', 'Staff'],
+            'create companies' => ['Owner'],
+            'update companies' => ['Owner'],
+            'view domains' => ['Owner', 'Staff'],
+            'create domains' => ['Owner'],
+            'update domains' => ['Owner'],
+            'view webpages' => ['Owner', 'Staff'],
+            'create webpages' => ['Owner'],
+            'update webpages' => ['Owner', 'Staff'],
+            'view places' => ['Owner', 'Staff'],
+            'create places' => ['Owner'],
+            'update places' => ['Owner'],
+            'view profile links' => ['Owner', 'Staff'],
+            'create profile links' => ['Owner'],
+            'update profile links' => ['Owner'],
+            'delete profile links'=> [],
+            'view keywords' => ['Owner', 'Staff'],
+            'create keywords' => ['Owner'],
+            'update keywords' => ['Owner'],
+            'view rankings' => ['Owner', 'Staff'],
+            'create rankings' => ['Owner'],
+            'update rankings' => ['Owner'],
+            'view search volumes' => ['Owner', 'Staff'],
+            'create search volumes' => ['Owner'],
+            'update search volumes' => ['Owner'],
+        ];
 
-            foreach ([
-                'view companies' => ['Owner', 'Staff'],
-                'create companies' => ['Owner'],
-                'update companies' => ['Owner'],
-                'view domains' => ['Owner', 'Staff'],
-                'create domains' => ['Owner'],
-                'update domains' => ['Owner'],
-                'view webpages' => ['Owner', 'Staff'],
-                'create webpages' => ['Owner'],
-                'update webpages' => ['Owner', 'Staff'],
-                'view places' => ['Owner', 'Staff'],
-                'create places' => ['Owner'],
-                'update places' => ['Owner'],
-                'view profile links' => ['Owner', 'Staff'],
-                'create profile links' => ['Owner'],
-                'update profile links' => ['Owner'],
-                'delete profile links'=> [],
-                'view keywords' => ['Owner', 'Staff'],
-                'create keywords' => ['Owner'],
-                'update keywords' => ['Owner'],
-                'view rankings' => ['Owner', 'Staff'],
-                'create rankings' => ['Owner'],
-                'update rankings' => ['Owner'],
-                'view search volumes' => ['Owner', 'Staff'],
-                'create search volumes' => ['Owner'],
-                'update search volumes' => ['Owner'],
-            ] as $name) {
-                app(Permission::class)::findOrCreate($name, null);
-            };
-        }
+        $this->createPermissions($permissions);
     }
 }

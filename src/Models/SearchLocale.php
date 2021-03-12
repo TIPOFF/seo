560430967dac98fa732bda97d72c280cda54660f
devjk1 <jinsik@omniadigital.io>
2021-03-12 12:31:38 -0500

    [TGER-96] 44 add Model SearchLocale

---
+++ src/Models/SearchLocale.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tipoff\Seo\Models;
+
+use Tipoff\Support\Models\BaseModel;
+use Tipoff\Support\Traits\HasCreator;
+use Tipoff\Support\Traits\HasPackageFactory;
+use Tipoff\Support\Traits\HasUpdater;
+
+class SearchLocale extends BaseModel
+{
+    use HasPackageFactory;
+    use HasCreator;
+    use HasUpdater;
+
+    public function rankings()
+    {
+        return $this->hasMany(Ranking::class);
+    }
+}

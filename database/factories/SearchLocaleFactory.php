38e3f7940dddcf4c60e10cf36ba3d150ce997ce8
devjk1 <jinsik@omniadigital.io>
2021-03-12 14:43:16 -0500

    [TGER-96] 44 add Factory SearchLocale

---
+++ database/factories/SearchLocaleFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tipoff\Seo\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SearchLocaleFactory extends Factory
+{
+    protected $model = SearchLocale::class;
+
+    public function definition()
+    {
+        return [
+            'serp_id'           => $this->faker->randomNumber(),
+            'google_id'         => $this->faker->randomNumber(),
+            'google_parent_id'  => $this->faker->randomNumber(),
+            'name'              => $this->faker->name,
+            'canonical_name'    => $this->faker->name,
+            'country_code'      => $this->faker->countryCode,
+            'target_type'       => 'DMA Region',
+            'reach'             => $this->faker->randomNumber(),
+            'latitude'          => $this->faker->latitude,
+            'longitude'         => $this->faker->longitude,
+            'creator_id'        => randomOrCreate(app('user')),
+            'updater_id'        => randomOrCreate(app('user')),
+        ];
+    }
+}

56e26b49b72f30b37fec0759005d501264779e1d
Drew Roberts <github@drewroberts.com>
2021-03-12 03:33:18 -0500

    Add seo group to search volume nova resource #45

--- src/Nova/SearchVolume.php
+++ src/Nova/SearchVolume.php
@@ -22,6 +22,8 @@ class SearchVolume extends BaseResource
     public static $search = [
         'id',
     ];
+    
+    public static $group = 'SEO';
 
     public function fieldsForIndex(NovaRequest $request)
     {

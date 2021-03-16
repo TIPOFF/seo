d076d40afdef536c95d29f842a772456b83b5021
Drew Roberts <github@drewroberts.com>
2021-03-15 23:38:01 -0400

    Resolve own class from service container

--- database/migrations/2020_01_01_140000_create_keywords_table.php
+++ database/migrations/2020_01_01_140000_create_keywords_table.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Tipoff\Seo\Models\Keyword;
 
 class CreateKeywordsTable extends Migration
 {
@@ -16,7 +15,7 @@ class CreateKeywordsTable extends Migration
             $table->string('phrase')->index()->unique();
             $table->string('type')->index(); // Example: 'Branded', 'Generic', 'Local'
 
-            $table->foreignIdFor(Keyword::class, 'parent_id')->nullable();
+            $table->foreignIdFor(app('keyword'), 'parent_id')->nullable();
             $table->foreignIdFor(app('user'), 'creator_id');
             $table->foreignIdFor(app('user'), 'updater_id');
             $table->timestamps();

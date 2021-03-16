040bf1cabe01049130cc00d9167e74bcad867b43
Drew Roberts <github@drewroberts.com>
2021-03-15 23:36:04 -0400

    Resolve classes from service provider

--- database/migrations/2020_01_01_120000_create_places_table.php
+++ database/migrations/2020_01_01_120000_create_places_table.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Tipoff\Seo\Models\Company;
-use Tipoff\Seo\Models\Webpage;
 
 class CreatePlacesTable extends Migration
 {
@@ -17,8 +15,8 @@ class CreatePlacesTable extends Migration
             $table->string('place_location')->unique(); // Google Places ID. Would prefer different field name.
             $table->string('name')->nullable();
             $table->string('maps_url')->nullable(); // URL for place's Google Maps page. (@todo convert to Webpage resource)
-            $table->foreignIdFor(Webpage::class)->nullable();
-            $table->foreignIdFor(Company::class)->nullable(); // Automatically pulled from webpage->domain->company on model saving
+            $table->foreignIdFor(app('webpage'))->nullable();
+            $table->foreignIdFor(app('company'))->nullable(); // Automatically pulled from webpage->domain->company on model saving
 
             $table->foreignIdFor(app('user'), 'creator_id')->nullable();
             $table->foreignIdFor(app('user'), 'updater_id')->nullable();

a8048239abfaf9c1f930ec61b31343eba628db88
devjk1 <jinsik@omniadigital.io>
2021-03-17 12:49:32 -0400

    Model Domain, helper method parseUrl() added

--- src/Models/Domain.php
+++ src/Models/Domain.php
@@ -19,4 +19,36 @@ class Domain extends BaseModel
     {
         return $this->belongsTo(Company::class);
     }
+
+    public function parseUrl(string $url)
+    {
+        $https = true;
+        $subdomain = '';
+        $name = '';
+        $tld = '';
+
+        $pieces = parse_url($url);
+
+        // host e.g. 'www.example.com'
+        $host = $pieces['host'];
+        $hostPieces = explode('.', $host);
+
+        // length 4 will not work, e.g. www.some.example.com
+        if (count($hostPieces) === 1) {
+            // error
+        }
+        else if (count($hostPieces) === 2) {
+            // $subdomain is an empty string
+            $name = $hostPieces[0];
+            $tld = $hostPieces[1];
+        } else {
+            $subdomain = $hostPieces[0];
+            $name = $hostPieces[1];
+            $tld = $hostPieces[2];
+        }
+
+        if ($pieces['scheme'] !== 'https') $https = false;
+
+        // use $https, $subdomain, $name, $tld
+    }
 }

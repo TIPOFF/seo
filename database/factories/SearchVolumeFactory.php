0f752ec7777bc7ac1b1146b8240dfedcf6555d1b
Patrick Breen <patrick.breen@quartzy.com>
2021-03-18 15:11:56 -0400

    Make sure queries is positive

--- database/factories/SearchVolumeFactory.php
+++ database/factories/SearchVolumeFactory.php
@@ -19,8 +19,8 @@ class SearchVolumeFactory extends Factory
             'provider'          => $this->faker->name,
             'keyword_id'        => Keyword::factory()->create()->id,
             'month'             => $this->faker->month,
-            'queries'           => $this->faker->randomNumber(),
-            'clicks'            => $this->faker->randomNumber(),
+            'queries'           => $this->faker->numberBetween(1, 1000),
+            'clicks'            => $this->faker->numberBetween(1, 1000),
             'creator_id'        => randomOrCreate(app('user')),
             'updater_id'        => randomOrCreate(app('user')),
         ];

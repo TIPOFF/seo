db64bfba76c10c89c6cf26bf95bec5c5887d95ed
Josh Torres <josht@omnia-app.org>
2021-04-03 23:48:59 -0700

    #84 - add test comments so tests are run

--- tests/Unit/Services/Keyword/CheckRankingTest.php
+++ tests/Unit/Services/Keyword/CheckRankingTest.php
@@ -8,11 +8,13 @@ use Tipoff\Seo\Tests\TestCase;
 
 class CheckRankingTest extends TestCase
 {
+    /** @test */
     public function check_local_results()
     {
         return $this->assertTrue(false);
     }
 
+    /** @test */
     public function check_global_results()
     {
         return $this->assertTrue(false);

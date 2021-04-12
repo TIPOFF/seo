d8854878a0afa8d7ff8efb88e947854ac035e55b
Patrick Breen <patrick.breen@quartzy.com>
2021-04-12 10:46:22 -0400

    Add string lengths

--- database/migrations/2020_01_01_160000_create_search_volumes_table.php
+++ database/migrations/2020_01_01_160000_create_search_volumes_table.php
@@ -10,15 +10,13 @@ class CreateSearchVolumesTable extends Migration
 {
     public function up()
     {
-        Schema::defaultStringLength(191);
-        
         Schema::create('search_volumes', function (Blueprint $table) {
             $table->id();
-            $table->string('engine')->index(); // Example: 'google', 'bing' (haha)
-            $table->string('provider')->index(); // Example: 'google search console', 'serpapi', 'moz', 'ahrefs'
+            $table->string('engine', 64)->index(); // Example: 'google', 'bing' (haha)
+            $table->string('provider', 64)->index(); // Example: 'google search console', 'serpapi', 'moz', 'ahrefs'
             $table->foreignIdFor(app('keyword'))->index();
-            $table->string('range')->index(); // Ex: 'month', 'week', 'day'. Will just use month for now as the ranges for search volumes
-            $table->string('range_value')->index(); // Example: 2021-05 for month range or a date for day range. Undecided what to use for week range. May convert to date field later.
+            $table->string('range', 32)->index(); // Ex: 'month', 'week', 'day'. Will just use month for now as the ranges for search volumes
+            $table->string('range_value', 32)->index(); // Example: 2021-05 for month range or a date for day range. Undecided what to use for week range. May convert to date field later.
 
             $table->integer('queries');
             $table->integer('clicks')->nullable(); // For search console only
@@ -26,7 +24,7 @@ class CreateSearchVolumesTable extends Migration
             $table->foreignIdFor(app('user'), 'creator_id')->nullable(); // Probably not ever used here, but just in case it is a requested run
             $table->foreignIdFor(app('user'), 'updater_id')->nullable(); // There may later be a few fields that are updatable, but most will be locked to not be editable
             $table->timestamps();
-            
+
             $table->unique(['engine', 'provider', 'keyword_id', 'range', 'range_value'], 'search_volume_unique');
         });
     }

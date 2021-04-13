697c5c64c273a81e250b65145f8ecae24642363b
arwaawan3 <arwaawan3@users.noreply.github.com>
2021-04-13 00:40:29 +0000

    Fix styling

--- src/Models/Keyword.php
+++ src/Models/Keyword.php
@@ -12,13 +12,14 @@ use Tipoff\Support\Traits\HasCreator;
 use Tipoff\Support\Traits\HasPackageFactory;
 use Tipoff\Support\Traits\HasUpdater;
 
-class Keyword extends BaseModel {
-
+class Keyword extends BaseModel
+{
     use HasPackageFactory;
     use HasCreator;
     use HasUpdater;
 
-    protected static function boot() {
+    protected static function boot()
+    {
         parent::boot();
 
         static::saving(function ($keyword) {
@@ -30,30 +31,33 @@ class Keyword extends BaseModel {
         });
     }
 
-    public function isBranded(): bool {
+    public function isBranded(): bool
+    {
         return $this->type == KeywordType::BRANDED;
     }
 
-    public function isGeneric(): bool {
+    public function isGeneric(): bool
+    {
         return $this->type == KeywordType::GENERIC;
     }
 
-    public function isLocal(): bool {
+    public function isLocal(): bool
+    {
         return $this->type == KeywordType::LOCAL;
     }
 
-    public function getRanking(): void {
+    public function getRanking(): void
+    {
         app(CheckRanking::class)($this);
     }
 
-    public function searchLocales() {
+    public function searchLocales()
+    {
         return $this->belongsToMany(app('search_locale'))->withTimestamps();
     }
 
     public function parent()
     {
-    return $this->belongsTo(app('keyword'), 'parent_id');
-
+        return $this->belongsTo(app('keyword'), 'parent_id');
     }
-
 }

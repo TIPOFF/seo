8cb1f179fab7f023fc35e8b5213ebce193e40512
Phuc Le <phuclh93@gmail.com>
2021-04-21 00:55:44 -0700

    Add Google Search Console to check keyword search volumes.

---
+++ src/Commands/CheckSearchVolumeCommand.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tipoff\Seo\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Bus;
+use SchulzeFelix\SearchConsole\Period;
+use SchulzeFelix\SearchConsole\SearchConsoleFacade as SearchConsole;
+use Tipoff\GoogleApi\Facades\GoogleOauth;
+use Tipoff\Seo\Jobs\CheckKeywordSearchVolumeJob;
+
+class CheckSearchVolumeCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'keywords:check-search-volume {url} {--limit=1000}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Pull data from Google Search Console and update keyword search volumes.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        // Get access token from Google Oauth package.
+        $accessToken = GoogleOauth::accessToken('google-console');
+
+        $batch = Bus::batch([])
+            ->name('check-keyword-search-volume-' . date('Y-m-d'))
+            ->allowFailures()
+            ->onQueue(config('seo.perform_on_queue.check_keyword_search_volume'))
+            ->dispatch();
+
+        /**
+         * Using SearchConsole Facade directly here,
+         * we might need to use registry pattern when we have more provider like Ahrefs or Semrush, etc.
+         */
+        SearchConsole::setAccessToken($accessToken->getAccessToken())
+            ->setQuotaUser('uniqueQuotaUserString')
+            ->searchAnalyticsQuery(
+                $this->argument('url'),
+                Period::create(now()->subDays(30), now()->subDays(2)),
+                ['query'],
+                [],
+                (int)$this->option('limit'),
+                'web',
+                'all',
+                'auto'
+            )
+            ->map(fn(array $payload) => $this->createCheckSearchVolumeJob($payload))
+            ->chunk(1000)
+            ->each(fn(Collection $jobs) => $batch->add($jobs));
+    }
+
+    /**
+     * @param array $payload
+     * @return CheckKeywordSearchVolumeJob
+     */
+    private function createCheckSearchVolumeJob(array $payload)
+    {
+        return new CheckKeywordSearchVolumeJob(
+            $payload,
+            'google',
+            'google-console',
+            date('Y-m')
+        );
+    }
+}
---
+++ src/Jobs/CheckKeywordSearchVolumeJob.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tipoff\Seo\Jobs;
+
+use Illuminate\Bus\Batchable;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Arr;
+use Tipoff\Seo\Models\Keyword;
+
+class CheckKeywordSearchVolumeJob implements ShouldQueue
+{
+    use InteractsWithQueue;
+    use Queueable;
+    use Dispatchable;
+    use Batchable;
+
+    private array $payload;
+    private string $engine;
+    private string $provider;
+    private $rangeValue;
+    private string $range;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(array $payload, string $engine, string $provider, $rangeValue, $range = 'month')
+    {
+        $this->payload = $payload;
+        $this->engine = $engine;
+        $this->provider = $provider;
+        $this->rangeValue = $rangeValue;
+        $this->range = $range;
+    }
+
+    /**
+     * Handle the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        /** @var Keyword $keyword */
+        $keyword = Keyword::where('phrase', Arr::get($this->payload, 'query'))->first();
+
+        if (!$keyword) {
+            return;
+        }
+
+        $keyword->searchVolume()->updateOrCreate(
+            [
+                'keyword_id' => $keyword->id,
+                'engine'     => $this->engine,
+                'provider'   => $this->provider,
+            ],
+            [
+                'range'       => $this->range,
+                'range_value' => $this->rangeValue,
+                'queries'     => Arr::get($this->payload, 'impressions'),
+                'clicks'      => Arr::get($this->payload, 'clicks')
+            ]
+        );
+    }
+}

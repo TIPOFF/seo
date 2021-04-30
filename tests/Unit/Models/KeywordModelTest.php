054e8b1d923302c1f7f39b30402d838076ab444a
dimayefremov <dima@buildmidwestern.com>
2021-04-30 21:44:07 +0300

    Add test coverage to keywordModel, isBranded, isLocal, isGeneric and parent methods #2

--- tests/Unit/Models/KeywordModelTest.php
+++ tests/Unit/Models/KeywordModelTest.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Tipoff\Seo\Tests\Unit\Models;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tipoff\Seo\Enum\KeywordType;
 use Tipoff\Seo\Models\Keyword;
 use Tipoff\Seo\Tests\TestCase;
 
@@ -17,5 +19,40 @@ class KeywordModelTest extends TestCase
     {
         $model = Keyword::factory()->create();
         $this->assertNotNull($model);
+    }
+
+    /** @test */
+    public function is_branded()
+    {
+        $keyword = Keyword::factory()->create(['type' => KeywordType::BRANDED]);
+        $this->assertTrue($keyword->isBranded());
+    }
+
+    /** @test */
+    public function is_generic()
+    {
+        $keyword = Keyword::factory()->create(['type' => KeywordType::GENERIC]);
+        $this->assertTrue($keyword->isGeneric());
+    }
+
+    /** @test */
+    public function is_local()
+    {
+        $keyword = Keyword::factory()->create(['type' => KeywordType::LOCAL]);
+        $this->assertTrue($keyword->isLocal());
+    }
+
+    /** @test */
+    public function get_parent()
+    {
+        $parentKeyword = Keyword::factory()->create();
+        $keyword = Keyword::factory()->create(['parent_id' => $parentKeyword->id]);
+
+        $this->assertInstanceOf(BelongsTo::class, $keyword->parent());
+        $this->assertEquals('parent_id', $keyword->parent()->getForeignKeyName());
+        $this->assertEquals('keywords.id', $keyword->parent()->getQualifiedParentKeyName());
+
+        $this->assertEquals($parentKeyword->id, $keyword->parent->id);
+        $this->assertEquals($parentKeyword->name, $keyword->parent->name);
     }
 }

4044afc8a5bb72acadf67ea2119efffc89f3e505
dimayefremov <dima@buildmidwestern.com>
2021-05-03 15:07:16 +0300

    Update PlaceModelTest, test for getResults #2

--- tests/Unit/Models/PlaceModelTest.php
+++ tests/Unit/Models/PlaceModelTest.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Tipoff\Seo\Tests\Unit\Models;
 
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tipoff\Seo\Models\Place;
+use Tipoff\Seo\Models\Result;
 use Tipoff\Seo\Tests\TestCase;
 
 class PlaceModelTest extends TestCase
@@ -17,5 +19,25 @@ class PlaceModelTest extends TestCase
     {
         $model = Place::factory()->create();
         $this->assertNotNull($model);
+    }
+
+    /** @test */
+    public function get_results()
+    {
+        $place = Place::factory()->create();
+        $results = Result::factory()->count(3)->create([
+            'resultable_id' => $place->id,
+            'resultable_type' => Place::class,
+        ]);
+
+        $place->results()->saveMany($results);
+
+        $this->assertInstanceOf(MorphMany::class, $place->results());
+        $this->assertEquals(Place::class, $place->results()->getMorphClass());
+        $this->assertEquals('resultable_id', $place->results()->getForeignKeyName());
+        $this->assertEquals('resultable_type', $place->results()->getMorphType());
+        $this->assertEquals('places.id', $place->results()->getQualifiedParentKeyName());
+
+        $this->assertCount($results->count(), $place->results()->get());
     }
 }

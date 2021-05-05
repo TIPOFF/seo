720511f0e2b77794b711ef2c6c5d3f0671ce710b
arwaawan <awan.arwa@aerocareusa.com>
2021-05-05 00:33:26 -0500

    fix companyuser nova resource and model

--- src/Models/CompanyUser.php
+++ src/Models/CompanyUser.php
@@ -15,14 +15,29 @@ class CompanyUser extends Pivot
     use HasCreator;
     use HasUpdater;
 
+    public $incrementing = true;
+
     protected static function boot()
     {
         parent::boot();
 
-        static::creating(function ($model) {
-            foreach ($model->pivotParent->users()->allRelatedIds() as $id) {
-                $model->pivotParent->users()->sync([$id => [ 'primary_contact' => false] ], false);
+        static::saving(function ($model) {
+            // Set previous contact is not primary if the current one is primary
+            if ($model->primary_contact) {
+                self::where('company_id', $model->company_id)
+                    ->where('id', '!=', $model->id)
+                    ->update(['primary_contact' => false]);
             }
         });
+    }
+
+    public function company()
+    {
+        return $this->belongsTo(app('company'));
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(app('user'));
     }
 }
--- src/Nova/CompanyUser.php
+++ src/Nova/CompanyUser.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Laravel\Nova\Fields\BelongsTo;
 use Laravel\Nova\Fields\Boolean;
 use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Http\Requests\NovaRequest;
 use Laravel\Nova\Panel;
 use Tipoff\Support\Nova\BaseResource;
@@ -21,13 +22,19 @@ class CompanyUser extends BaseResource
     public static $search = [
         'id',
     ];
-    
+
     public static $group = 'Access';
 
     public function fieldsForIndex(NovaRequest $request)
     {
         return array_filter([
             ID::make()->sortable(),
+            Text::make('Company', 'company.id', function () {
+                return $this->company->name;
+            }),
+            Text::make('User', 'user.id', function () {
+                return $this->user->full_name;
+            }),
         ]);
     }
 
